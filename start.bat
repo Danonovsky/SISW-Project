20516e57339c59b98b0c4878951c55ac4c72b919
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2023-01-09 22:31:28 +0100

    Już ogródek, już prawie witam się z gąską

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 kubectl apply -f postgres-deployment.yaml,identity-deployment.yaml,app-deployment.yaml,gw-deployment.yaml,frontend-deployment.yaml
-kubectl apply -f identity-service.yaml,app-service.yaml,gw-service.yaml,frontend-service.yaml
+kubectl apply -f postgres-service.yaml,identity-service.yaml,app-service.yaml,gw-service.yaml,frontend-service.yaml
